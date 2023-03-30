5afb9a810cf236d5e9122629d3d3656f7bb6415c
LucasVz <lucastadeuvaz@gmail.com>
2023-03-30 15:40:35 -0300

    add: project architecture

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import List from "./pages/List";
+import Map from "./pages/Map";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
